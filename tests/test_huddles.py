f36d28784b3c48e4c67ae7bd5604860e07d174ed
Artashes <100474936+Artashes23@users.noreply.github.com>
2023-09-22 17:10:42 +0400

    Update test_huddles.py

--- tests/test_huddles.py
+++ tests/test_huddles.py
@@ -7,5 +7,5 @@ import os
 class Test_Huddles(Check_Huddles):
     def test_contributer_in_huddles_TC_9(self,setup):
         self.browser = setup
-        self.browser.get(os.getenv('URL'))
+        self.browser.get(os.getenv("URL"))
         self.check_contributor_in_huddles()
