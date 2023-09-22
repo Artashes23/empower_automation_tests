21982167d9a34946da195c150dc0d9ac4f9dc76b
Artashes <100474936+Artashes23@users.noreply.github.com>
2023-09-22 12:12:33 +0400

    Update test_initiatives.py

--- tests/test_initiatives.py
+++ tests/test_initiatives.py
@@ -12,12 +12,12 @@ class Test_Create_Delete_Init(Check_Create_Init):
     
     def test_create_delete_init_TC_1(self,setup):
         self.browser = setup
-        self.browser.get(os.getenv('Url'))
+        self.browser.get(os.getenv('URL'))
         self.check_create_delete()
 
     def test_create_init_without_title_TC_2(self,setup):
         self.browser = setup
-        self.browser.get(os.getenv('Url'))
+        self.browser.get(os.getenv('URL'))
         self.check_create_init_without_title()
 
 
