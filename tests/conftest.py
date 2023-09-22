c4319acde4a0cbd00f1f0319bf79ed6a253858c3
Artashes <100474936+Artashes23@users.noreply.github.com>
2023-09-22 13:59:32 +0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,9 +32,9 @@ chromedriver_path = "/usr/local/bin/chromedriver"
 def setup():
     chrome_options = Options()
     chrome_options.binary_location = "/usr/bin/google-chrome"
-    #chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless")
+    #chrome_options.add_argument("--no-sandbox")
+    #chrome_options.add_argument("--disable-dev-shm-usage")
     browser = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
         
     
