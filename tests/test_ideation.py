ab7f6a29c2e900957c0d7b9dd7269f79d68c3879
Artashes <100474936+Artashes23@users.noreply.github.com>
2023-09-22 12:12:21 +0400

    Update test_ideation.py

--- tests/test_ideation.py
+++ tests/test_ideation.py
@@ -9,22 +9,22 @@ class Test_Ideations(Check_Ideations):
     load_dotenv
     def test_create_delete_ideations_TC_6(self,setup):
         self.browser = setup
-        self.browser.get(os.getenv('Url'))
+        self.browser.get(os.getenv('URL'))
         self.check_create_delete()
 
     def test_without_switching_valid_error_TC_7(self,setup):
         self.browser = setup
-        self.browser.get(os.getenv('Url'))
+        self.browser.get(os.getenv('URL'))
         self.create_ideation_without_switching_any_switch()
 
     def test_change_anything_after_publishing_ideation_TC_8(self,setup):
         self.browser = setup
-        self.browser.get(os.getenv('Url'))
+        self.browser.get(os.getenv('URL'))
         self.change_anything_after_publishing_ideation()
 
     def test_ideation_results_14(self,setup):
         self.browser = setup
-        self.browser.get(os.getenv('Url'))
+        self.browser.get(os.getenv('URL'))
         self.check_ideation_result()
     
     
