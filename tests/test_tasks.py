b57fe3497a6543392280359b504c495c054b41ae
Artashes <100474936+Artashes23@users.noreply.github.com>
2023-09-22 17:13:48 +0400

    Update test_tasks.py

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -7,22 +7,22 @@ class Test_Tasks(Check_Tasks):
     
     def test_create_tasks_TC_11(self,setup):
         self.browser = setup
-        self.browser.get(os.getenv('URL'))
+        self.browser.get(os.getenv("URL"))
         self.check_create_task()
         
     def test_task_without_name_TC_12(self,setup):
         self.browser = setup
-        self.browser.get(os.getenv('URL'))
+        self.browser.get(os.getenv("URL"))
         self.check_create_task_without_name()
 
     def test_task_edit_TC_13(self,setup):
         self.browser = setup
-        self.browser.get(os.getenv('URL'))
+        self.browser.get(os.getenv("URL"))
         self.check_tasks_edit()
 
     def test_task_due_complete_interval(self,setup):
         self.browser = setup
-        self.browser.get(os.getenv('URL'))
+        self.browser.get(os.getenv("URL"))
         self.check_due_complete_interval()
 
     
